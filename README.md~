8695df481ac466e1a715aa38cf32e4e5090f5341
PeterJi-Ventrify <peter.ji@ventrify.ca>
2022-06-09 15:51:57 -0600

    This is the first commit to this repo

---
+++ README.md~
@@ -0,0 +1,2 @@
+# C-Tutorial
+This repo contains my take on the practices in the book "The C Programming Language"
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(void){
+	int test = 0;
+	printf("Print test: %d", test);
+	return;
+}
